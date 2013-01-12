11eef9319a86d8dba1d54b6730ef609325e8362c
J0shua <criticalmail@hotmail.com>
2013-01-13 00:54:22 +1100

    initial prototype

---
+++ Sunswift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sunswift
+//
+//  Created by Joshua CHIN on 7/01/13.
+//  Copyright (c) 2013 UNSW Solar Racing Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarContoller;
+
+@end
